376c717dea1cf9b859cf7566c47962563ea7dba2
Jakub Kupczyk <jkbkupczyk@gmail.com>
2020-09-03 22:41:21 +0200

    Moved repo class to /api

--- src/main/java/pl/kupczyk/springoo/api/SpringooRepository.java
+++ src/main/java/pl/kupczyk/springoo/api/SpringooRepository.java
@@ -1,7 +1,8 @@
-package pl.kupczyk.springoo.model;
+package pl.kupczyk.springoo.api;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.kupczyk.springoo.model.Room;
 
 @Repository
 public interface SpringooRepository extends CrudRepository<Room, Long> {
