23d8aa86ce3c6fd765456af927557df5f2defe5c
Jakub Kupczyk <jkbkupczyk@gmail.com>
2020-09-18 23:06:58 +0200

    Added new endpoints, service and repo layer

--- src/main/java/pl/kupczyk/springoo/api/SpringooRepository.java
+++ src/main/java/pl/kupczyk/springoo/api/SpringooRepository.java
@@ -1,5 +1,6 @@
 package pl.kupczyk.springoo.api;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.kupczyk.springoo.model.Room;
@@ -7,4 +8,5 @@ import pl.kupczyk.springoo.model.Room;
 @Repository
 public interface SpringooRepository extends CrudRepository<Room, Long> {
 
+    @Query(value = "SELECT * FROM RESERVATION rs WHERE rs.")
 }
